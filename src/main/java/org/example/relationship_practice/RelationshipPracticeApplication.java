b62bec10dd55c95215e932ca340e55f218c1a05b
akilaisuru <akilaisurukahingala@gmail.com>
2026-07-20 15:32:50 +0530

    first commit

---
+++ src/main/java/org/example/relationship_practice/RelationshipPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.relationship_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationshipPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelationshipPracticeApplication.class, args);
+    }
+
+}
